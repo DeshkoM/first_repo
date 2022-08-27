6b040f62ac5de9502097a558d3debea64552713d
DeshkoM <deshko86@gmail.com>
2022-08-27 12:20:12 +0300

    1st msg

---
+++ src/main/kotlin/ua/deshko/first_project/FirstProjectApplication.kt
@@ -0,0 +1,11 @@
+package ua.deshko.first_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<FirstProjectApplication>(*args)
+}
