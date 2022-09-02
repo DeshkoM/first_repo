e9342c0c7da7662b5183ad38a988a37ac2b8ae08
DeshkoM <deshko86@gmail.com>
2022-09-02 12:37:34 +0300

    msg 02.09.2022

--- src/main/kotlin/ua/deshko/first_project/math.kt
+++ src/main/kotlin/ua/deshko/first_project/math.kt
@@ -1,23 +1,25 @@
 
-fun summ (a:Int, b:Int):Int {
-    var summ = a + b
-    return summ
-}
-fun difference (a:Int,b:Int):Int{
-    var raznost = a - b
-    return raznost
-}
-
-fun segmentation (a:Float,b: Float):Float{
-    var delenie = a / b
-    return delenie
-}
-fun exponentiate (a: Int, b: Int):Int{
-    var stepen = a
-    for (x in 1..b-1){
-        stepen = stepen * a
+fun summ (a:Int, b:Int):Int = a + b
+fun difference (a:Int,b:Int):Int = a - b
+fun multipl (a:Int, b: Int) :Int = a * b
+fun segmentation (a:Int,b: Int):Float{
+    val a2 = a.toFloat()
+    val b2 = b.toFloat()
+    if (b2 == 0.0F){
+        return 0.0F
     }
-    return stepen
+    else {
+        return a2/b2
+    }
+}
+fun exponentiate (a: Int, b: Int): Float {
+    var stepen = a.toFloat()
+    var x = 1
+        while (x < b) {
+            stepen = stepen * a
+            x++
+        }
+    if (b>0) return stepen else if(b<0) return 1/stepen else return 1.0F
 }
 
 fun factorial (a:Int):Int{
@@ -30,9 +32,76 @@ fun factorial (a:Int):Int{
     return factorial
 }
 
-fun triangle (a:Int, b: Int): Boolean {
-    if (a != b) {
+fun is_triangle (a:Int, b: Int, c:Int):Boolean{
+    if (summ(a,b)>c && summ(a,c)>b && summ(b,c)>a){
+    return true
+    } else {
         return false
-    } else {return true}
+    }
 }
+fun choose_max (a: Int, b: Int, c: Int):Int {
+    val arr = arrayOf(a,b,c)
+    var max = if (arr[0]> arr[1] && arr[0]>arr[2]) {
+        return arr[0].toInt()
+    }
+    else if (arr[1]> arr[2] && arr[1]>arr[0]){
+        return arr[1].toInt()
+    }
+    else{
+        return arr[2].toInt()
+    }
+}
+fun choose_min (a: Int, b: Int, c: Int):Int {
+    val arr = arrayOf(a,b,c)
+    var min = if (arr[0]< arr[1] && arr[0]<arr[2]) {
+        return arr[0].toInt()
+    }
+    else if (arr[1]< arr[2] && arr[1]<arr[0]){
+        return arr[1].toInt()
+    }
+    else{
+        return arr[2].toInt()
+    }
+}
+fun choose_middle (a: Int, b: Int, c: Int):Int {
+    val arr = arrayOf(a,b,c)
+    var min = if (arr[0] > arr[1] && arr[0] < arr[2] || arr[0] < arr[1] && arr[0] > arr[2]) {
+        return arr[0].toInt()
+    }
+    else if (arr[1]> arr[2] && arr[1] < arr[0] || arr[1]<arr[2] && arr[1]>arr[0]){
+        return arr[1].toInt()
+    }
+    else if (arr[2]> arr[0] && arr[2] < arr[1] || arr[2] < arr[0] && arr[2] > arr[1]){
+        return arr[2].toInt()
+    }
+    else if (arr[0]==arr[1] && arr[0] != arr[2]){
+        return arr[0].toInt()
+    }
+    else if (arr[1]==arr[2] && arr[1] != arr[0]){
+        return arr[1].toInt()
+    }
+    else if (arr[2]==arr[0] && arr[2] != arr[1]){
+        return arr[2].toInt()
+    }
+    else return arr[0].toInt()
+
+}
+fun choose_type_triangle (a: Int,b: Int, c: Int): String{
+    val triangle = is_triangle(a, b, c)
+    val hipotenus = choose_max(a, b, c)
+    val min = choose_min(a, b, c)
+    val middle = choose_middle(a, b, c)
+    if (triangle == true && exponentiate(min,2) + exponentiate(middle,2) == exponentiate(hipotenus,2)){
+        return "Треугольник существует, треугольник прямоугольный"
+    }
+    else if (triangle == true && min == middle && min == hipotenus){
+        return "Треугольник существует, треугольник равносторонний"
+    }
+    else if (triangle == true && multipl(a,b)+multipl(b,c) > multipl(a,c)){
+        return "Треугольник существует, треугольник разносторонний"
+    }
+    else return "Другой треугольник"
+}
+
+
 
--- src/test/kotlin/ua/deshko/first_project/MathKtTest.kt
+++ src/test/kotlin/ua/deshko/first_project/MathKtTest.kt
@@ -1,75 +1,134 @@
 package ua.deshko.first_project
 
+import choose_max
+import choose_middle
+import choose_min
+import choose_type_triangle
 import segmentation
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import difference
 import exponentiate
 import factorial
+import is_triangle
 import summ
-import triangle
+
 
 class MathKtTest {
 
     @Test
-    fun `Test1,1 1+1 = 2 ` (){
-        val dannie = summ(1,1)
-        assertEquals(2,dannie)
+    fun `Test1 1+1 = 2 `() {
+        val dannie = summ(1, 1)
+        assertEquals(2, dannie)
     }
+
     @Test
-    fun `Test1,2 333 + 333 = 666` (){
-        val dannie = summ(333,333)
-        assertEquals(666,dannie)
+    fun `Test2 333 + 333 = 666`() {
+        val dannie = summ(333, 333)
+        assertEquals(666, dannie)
     }
+
     @Test
-    fun `Test2,1 2 - 1 = 1 ` (){
-        val dannie = difference(2,1)
+    fun `Test3 2 - 1 = 1 `() {
+        val dannie = difference(2, 1)
         assertEquals(1, dannie)
     }
+
     @Test
-    fun `Test2,2 785 - 234 = 551`(){
-        val dannie = difference(785,234)
+    fun `Test4 785 - 234 = 551`() {
+        val dannie = difference(785, 234)
         assertEquals(551, dannie)
     }
+
     @Test
-    fun `Test3,1 1 delim na  2 = 0,5` (){
-        val dannie = segmentation(1F,2F)
+    fun `Test5 1 delim na  2 = 0,5`() {
+        val dannie = segmentation(1, 2)
         assertEquals(0.5F, dannie)
     }
+
     @Test
-    fun `Test3,2 8375 delim na 33,5 = 250`(){
-        val dannie = segmentation(8375F,33.5F)
-        assertEquals(250.0F,dannie)
+    fun `Test6 25 delim na 0 = 0,0`() {
+        val dannie = segmentation(25, 0)
+        assertEquals(0.0F, dannie)
     }
+
     @Test
-    fun `Test4,1 2 v stepeni 10 = 1024`(){
-        val dannie = exponentiate(2,10)
-        assertEquals(1024,dannie)
+    fun `Test7 20 delim na -10 = -2`() {
+        val dannie = segmentation(20, -10)
+        assertEquals(-2F, dannie)
     }
+
     @Test
-    fun `Test 4,2 12 v stepeni 5 = 248832` () {
-        val dannie = exponentiate(12,5)
-        assertEquals(248832,dannie)
+    fun `Test8 2 v stepeni 10 = 1024`() {
+        val dannie = exponentiate(2, 10)
+        assertEquals(1024F, dannie)
     }
+
     @Test
-    fun `Test 5,1 6! = 720`(){
+    fun `Test9 2 v stepeni -1 = 0,5`() {
+        val dannie = exponentiate(2, -1)
+        assertEquals(0.5F, dannie)
+    }
+
+    @Test
+    fun `Test10 100 v stepeni 0 = 1`() {
+        var dannie = exponentiate(100, 0)
+        assertEquals(1F, dannie)
+    }
+
+    @Test
+    fun `Test11 6! = 720`() {
         val dannie = factorial(6)
         assertEquals(720, dannie)
     }
+
     @Test
-    fun `Test 5,2 10! = 3628800`(){
+    fun `Test12 10! = 3628800`() {
         val dannie = factorial(10)
-        assertEquals(3628800,dannie)
+        assertEquals(3628800, dannie)
     }
     @Test
-    fun `Test 6,1 a= 2, b=3 = Ne ravnostoronniy`(){
-        val dannie = triangle(2,3)
+    fun `Test13 Triangle is exist`(){
+        var dannie = is_triangle(4,4,5)
+        assertTrue(dannie)
+    }
+    @Test
+    fun `Test14 Triangle is not exist`(){
+        var dannie = is_triangle(10,4,5)
         assertFalse(dannie)
     }
     @Test
-    fun `Test 6,2 a= 5, b=5 = Ravnostoronniy` (){
-        val dannie = triangle(5,5)
-        assertTrue(dannie)
+    fun `Test15 Choose max int a=3,b=4,c=5   max 5`(){
+        var dannie = choose_max(3,4,5)
+        assertEquals(5,dannie)
+    }
+    @Test
+    fun `Test16 Choose min int a=3,b=4,c=5   min 3`(){
+        val dannie = choose_min(3,4,5)
+        assertEquals(3,dannie)
+    }
+    @Test
+    fun `Test17 Choose middle int a=3,b=4,c=5   middle 4`() {
+        val dannie = choose_middle(3, 4, 5)
+        assertEquals(4,dannie)
+    }
+    @Test
+    fun `Test18 equilateral triangle a=4,b=4,c=4  ` (){
+        var dannie =choose_type_triangle(4,4,4)
+        assertEquals("Треугольник существует, треугольник равносторонний",dannie)
+    }
+    @Test
+    fun `Test19 right triangle a=3, b=4, c=5`(){
+        var dannie = choose_type_triangle(3,4,5)
+        assertEquals("Треугольник существует, треугольник прямоугольный",dannie)
+    }
+    @Test
+    fun `Test20 scalene triangle a=3, b=5, c=6`(){
+        var dannie = choose_type_triangle(6,5,3)
+        assertEquals("Треугольник существует, треугольник разносторонний",dannie)
     }
 }
+
+
+
 
